e1366bb0d7e67c9683503c278ce1c5336b8a823a
Mark <marklindsay@protonmail.com>
2021-11-28 13:22:49 -0700

    got the args to work.

--- keyboards/ergodash/rev1/keymaps/marklindsay/keymap.c
+++ keyboards/ergodash/rev1/keymaps/marklindsay/keymap.c
@@ -37,10 +37,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_EXPRMNT] = LAYOUT_wrapper( \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                   KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,  \
-    KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                   KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME,                   KC_DEL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TG_NMPD,                   XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_ESC,  ________________NUMBER_LEFT________________, KC_MINS,                   KC_EQL,  ________________NUMBER_RIGHT_______________, KC_EQL,  \
+    KC_GRV,  _________________QWERTY_L1_________________, KC_LBRC,                   KC_RBRC, _________________QWERTY_R1_________________, KC_BSLS, \
+    KC_TAB,  _________________QWERTY_L2_________________, KC_HOME,                   KC_DEL,  _________________QWERTY_R2_________________, \
+    KC_LSFT, _________________QWERTY_L3_________________, TG_NMPD,                   XXXXXXX, _________________QWERTY_R3_________________, KC_RSFT, \
     KC_LCTL, KC_LGUI, KC_LALT, KC_ENT,  KC_SPC,  LOWER,   KC_BSPC,                   KC_BSPC, RAISE,   KC_SPC,  KC_ENT,  KC_RALT, KC_RGUI, KC_RCTL  \
   ),
 
