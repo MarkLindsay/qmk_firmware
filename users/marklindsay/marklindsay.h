9ee4c10b2011d33fcad3b7eb93136d46fec9ed07
Mark Lindsay <marklindsay@protonmail.com>
2021-11-28 12:30:58 -0700

    added wrappers file, tried to use it in the linux layout.

--- users/marklindsay/marklindsay.h
+++ users/marklindsay/marklindsay.h
@@ -1,7 +1,9 @@
 #ifndef USERSPACE
 #define USERSPACE
 
-#include "quantum.h"
+//#include "quantum.h"
+#include QMK_KEYBOARD_H
+#include "wrappers.h" // keymap blocks are defined in wrappers.h and then marklindsay.h is included in the keymap.c 
 
 void my_custom_function(void);
 
--- users/marklindsay/rules.mk
+++ users/marklindsay/rules.mk
@@ -1,1 +1,6 @@
 SRC +=marklindsay.c
+
+ifdef RGB_ENABLE
+  # Include my fancy rgb functions source here
+  #SRC += cool_rgb_stuff.c
+endif
